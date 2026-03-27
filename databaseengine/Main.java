4b725ae5eca0aebffed3c90ee34f69eba1aa1405
sherlinvst <sherlinticsay06@gmail.com>
2026-03-27 19:14:40 +0800

    Created College model and modified Main.java for testing

--- databaseengine/Main.java
+++ databaseengine/Main.java
@@ -1,10 +1,13 @@
 package databaseengine;
 
+import java.sql.Date;
+
 import databaseengine.backend.StartDatabase;
+import databaseengine.backend.model.Student;
 
 public class Main {
     public static void main(String[] args) {
         StartDatabase startDb = new StartDatabase();
-        startDb.getDb().getStudent().viewStudents();
+        startDb.getDb().getStudent().deleteStudent(21);
     }
 }
