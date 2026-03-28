464f79f23ced84f3b4e386b8434f301fb0172cf1
sherlinvst <sherlinticsay06@gmail.com>
2026-03-28 18:03:53 +0800

    Added DepartmentService in Database

--- databaseengine/backend/Database.java
+++ databaseengine/backend/Database.java
@@ -1,16 +1,24 @@
 package databaseengine.backend;
 
 import java.sql.Connection;
+
+import databaseengine.backend.service.DepartmentService;
 import databaseengine.backend.service.StudentService;
 
 public class Database {
     private StudentService student;
+    private DepartmentService department;
 
     public Database(Connection connect){
         this.student = new StudentService(connect);
+        this.department = new DepartmentService(connect);
     }
 
     public StudentService getStudent(){
         return student;
+    }
+
+    public DepartmentService getDepartment(){
+        return department;
     }
 }
